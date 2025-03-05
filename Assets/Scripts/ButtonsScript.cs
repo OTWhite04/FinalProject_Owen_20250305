40aa2d712e9da6a9dd5ba2fc05e03f64eb7a199f
OTWhite04 <owen.t.white2005@gmail.com>
2025-03-05 11:55:58 -0400

    creating buttons script

--- Assets/Scripts/ButtonsScript.cs
+++ Assets/Scripts/ButtonsScript.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class ButtonsScript : MonoBehaviour
 {
+
+    public bool isPaused;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,19 @@ public class ButtonsScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
     }
 }
